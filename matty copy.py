7f00fc34ececd855c8fc38420cde47f91ec86220
Gastonight <matthew.r.gaston@gmail.com>
2021-07-04 15:45:24 +0100

    just a test ok nothing pro

---
+++ matty copy.py
@@ -0,0 +1,24 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=2)
+from time import sleep
+
+kit.servo[0].angle = 0
+kit.servo[1].angle = 0
+
+sleep(2)
+
+kit.servo[0].angle = 180
+
+sleep(2)
+
+kit.servo[1].angle = 180
+
+sleep(1)
+
+kit.servo[0].angle = 0
+kit.servo[1].angle = 0
+
+sleep(1)
+
+kit.servo[0].angle = 180
+kit.servo[1].angle = 180
