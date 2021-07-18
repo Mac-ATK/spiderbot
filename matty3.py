52e0460e9cf1b1889ef26d3fdcda37a71f1db36d
Gastonight <matthew.r.gaston@gmail.com>
2021-07-18 15:49:08 +0100

    matty poo

--- matty3.py
+++ matty3.py
@@ -8,13 +8,31 @@ def leg1(angle1, angle2, angle3):
     kit.servo[2].angle = angle2
 
 def leg2(angle1, angle2, angle3):
-    kit.servo[3].angle = angle1
+    kit.servo[3].angle = 180 - angle1
     kit.servo[4].angle = angle2
     kit.servo[5].angle = angle2
 
+maxval = 10
+minval = 170
 
-leg1(90, 90, 90)
-leg2(90, 90, 90)
+
+leg1(maxval, 90, 90)
+leg2(maxval, 90, 90)
+
+sleep(2)
+
+leg1(minval, 90, 90)
+leg2(minval, 90, 90)
+
+sleep(2)
+
+leg1(maxval, 90, 90)
+leg2(maxval, 90, 90)
+
+sleep(2)
+
+leg1(minval, 90, 90)
+leg2(minval, 90, 90)
 
 
 
