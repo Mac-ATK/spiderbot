ccedfc381e704de1aa68f675b12d936add6494d1
Gastonight <matthew.r.gaston@gmail.com>
2021-09-05 16:06:20 +0100

    yes

--- calibrate.py
+++ calibrate.py
@@ -2,55 +2,40 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 from time import sleep
 
-hipmax = 50
-#hipmin = 180
-hipmin = 130
-
-
-#ldown = 0
-ldown = 0
-#lup = 180
-lup = 140
-
 def leg1(angle1, angle2, angle3):
     kit.servo[0].angle = angle1
     kit.servo[1].angle = angle2
-    kit.servo[2].angle = angle3
+    kit.servo[2].angle = angle2
 
 def leg2(angle1, angle2, angle3):
     kit.servo[3].angle = 180 - angle1
     kit.servo[4].angle = 180 - angle2
-    kit.servo[5].angle = 180 - angle3
+    kit.servo[5].angle = 180 - angle2
 
 def leg3(angle1, angle2, angle3):
     kit.servo[6].angle = 180 - angle1
     kit.servo[7].angle = 180 - angle2
-    kit.servo[8].angle = 180 - angle3
+    kit.servo[8].angle = 180 - angle2
 
 def leg4(angle1, angle2, angle3):
     kit.servo[9].angle = angle1
     kit.servo[10].angle = angle2
-    kit.servo[11].angle = angle3
+    kit.servo[11].angle = angle2
 
-angle_leg = lup
-angle_hip = hipmin
+maxval = 50
+minval = 180
 
-leg1(hipmin, 180, 0)
-leg2(hipmin, 90, 90)
-leg3(90, 90, 90)
-leg4(140, 90, 90)
+ldown = 0
+lup = 180
 
-sleep(0.2)
-leg1(0, 180, 0)
+sleeptime = 1
 
-sleep(0.2)
-leg1(180, 180, 0)
+testno = 110
 
-sleep(0.2)
-leg1(0, 180, 0)
-
-sleep(0.2)
-leg1(180, 180, 0)
+leg1(minval, lup, lup)
+leg2(minval, lup, lup)
+leg3(minval, lup, lup)
+leg4(minval, lup, lup)
 
 
 
@@ -58,5 +43,12 @@ leg1(180, 180, 0)
 
 
 
-    
-    
+
+
+
+
+
+
+
+
+
