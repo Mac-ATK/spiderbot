77b4e5e2374b240233e26a04eb59b67e4b376c42
Gastonight <matthew.r.gaston@gmail.com>
2021-09-12 15:26:05 +0100

    lol

--- matty2.py
+++ matty2.py
@@ -3,8 +3,8 @@ kit = ServoKit(channels=8)
 from time import sleep
 
 def leg1(angle1, angle2):
-    kit.servo[0].angle = angle1
-    kit.servo[1].angle = angle2
+    kit.servo[0].angle = 180 - angle1
+    kit.servo[1].angle = 180 - angle2
 
 
 leg1(0, 0)
--- matty3.py
+++ matty3.py
@@ -3,14 +3,14 @@ kit = ServoKit(channels=8)
 from time import sleep
 
 def leg1(angle1, angle2, angle3):
-    kit.servo[0].angle = angle1
-    kit.servo[1].angle = angle2
-    kit.servo[2].angle = angle2
+    kit.servo[0].angle = 180 - angle1
+    kit.servo[1].angle = 180 - angle2
+    kit.servo[2].angle = 180 - angle3
 
 def leg2(angle1, angle2, angle3):
-    kit.servo[3].angle = 180 - angle1
-    kit.servo[4].angle = 180 - angle2
-    kit.servo[5].angle = 180 - angle2
+    kit.servo[3].angle = angle1
+    kit.servo[4].angle = angle2
+    kit.servo[5].angle = angle3
 
 maxval = 50
 minval = 170
