8c9f44c86f628249dfa7491f3cc9421cd5457246
Gastonight <matthew.r.gaston@gmail.com>
2021-09-12 14:55:39 +0100

    pie

--- calibrate.py
+++ calibrate.py
@@ -3,24 +3,24 @@ kit = ServoKit(channels=16)
 from time import sleep
 
 def leg1(angle1, angle2, angle3):
-    kit.servo[0].angle = angle1
-    kit.servo[1].angle = angle2
-    kit.servo[2].angle = angle2
+    kit.servo[0].angle = 180 - angle1
+    kit.servo[1].angle = 180 - angle2
+    kit.servo[2].angle = 180 - angle3
 
 def leg2(angle1, angle2, angle3):
-    kit.servo[3].angle = 180 - angle1
-    kit.servo[4].angle = 180 - angle2
-    kit.servo[5].angle = 180 - angle2
+    kit.servo[3].angle = angle1
+    kit.servo[4].angle = angle2
+    kit.servo[5].angle = angle3
 
 def leg3(angle1, angle2, angle3):
     kit.servo[6].angle = 180 - angle1
     kit.servo[7].angle = 180 - angle2
-    kit.servo[8].angle = 180 - angle2
+    kit.servo[8].angle = 180 - angle3
 
 def leg4(angle1, angle2, angle3):
     kit.servo[9].angle = angle1
     kit.servo[10].angle = angle2
-    kit.servo[11].angle = angle2
+    kit.servo[11].angle = angle3
 
 maxval = 50
 minval = 180
@@ -37,6 +37,10 @@ leg2(minval, lup, lup)
 leg3(minval, lup, lup)
 leg4(minval, lup, lup)
 
+#leg1(minval, ldown, ldown)
+#leg2(minval, ldown, ldown)
+#leg3(minval, ldown, ldown)
+#leg4(minval, ldown, ldown)
 
 
 
