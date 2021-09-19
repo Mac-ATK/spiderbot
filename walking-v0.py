e62a8a001b649037002fe3459994bce40474836e
Mac-ATK <mac.bowley@gmail.com>
2021-09-19 14:08:48 +0100

    adding walking animation

---
+++ walking-v0.py
@@ -0,0 +1,56 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+from time import sleep
+import threading
+
+def leg1(angle1, angle2, angle3):
+    kit.servo[0].angle = 180 - angle1
+    kit.servo[1].angle = 180 - angle2
+    kit.servo[2].angle = 180 - angle3
+
+def leg2(angle1, angle2, angle3):
+    kit.servo[3].angle = angle1
+    kit.servo[4].angle = angle2
+    kit.servo[5].angle = angle3
+
+def leg3(angle1, angle2, angle3):
+    kit.servo[6].angle = 180 - angle1
+    kit.servo[7].angle = 180 - angle2
+    kit.servo[8].angle = 180 - angle3
+
+def leg4(angle1, angle2, angle3):
+    kit.servo[9].angle = angle1
+    kit.servo[10].angle = angle2
+    kit.servo[11].angle = angle3
+
+hipin = 180
+hipout = 100
+
+ldown = 0
+lup = 180
+
+leg1(hipin, lup, lup)
+
+for i in range(8):
+    diff = hipin - hipout
+    for i in range(4):
+        leg1(hipin+(diff/4), lup, lup)
+    for i in range(4):
+        leg1(hipout-(diff/4), lup, lup) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
