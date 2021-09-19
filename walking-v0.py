d549efae9cee86384e0750d1e7967519b6847617
Mac-ATK <mac.bowley@gmail.com>
2021-09-19 15:22:56 +0100

    knees and ankles

--- walking-v0.py
+++ walking-v0.py
@@ -36,32 +36,45 @@ current_2 = hipin
 current_3 = hipin-((hipdiff/3)*2)
 current_4 = hipin-((hipdiff/3)*2)
 
-leg1(current_1, lup, lup)
-leg2(current_2, lup, lup)
-leg3(current_3, lup, lup)
-leg4(current_4, lup, lup)
+leg_1 = ldown
+leg_2 = ldown
+leg_3 = ldown
+leg_4 = ldown
+
+leg1(current_1, ldown, ldown)
+leg2(current_2, ldown, ldown)
+leg3(current_3, ldown, ldown)
+leg4(current_4, ldown, ldown)
 
 for i in range(32):
     if current_1 == hipin:
         current_1 = hipout
+        leg_1 = lup
     else:
         current_1 += (hipdiff/3)
+        leg_1 = ldown
     if current_2 == hipout:
         current_2 = hipin
+        leg_2 = lup
     else:
         current_2 -= (hipdiff/3)
+        leg_2 = ldown
     if current_3 == hipout:
         current_3 = hipin
+        leg_3 = lup
     else:
         current_3 -= (hipdiff/3)
+        leg_3 = ldown
     if current_4 == hipin:
         current_4 = hipout
+        leg_4 = lup
     else:
         current_4 += (hipdiff/3)
-    leg1(current_1, lup, lup)
-    leg2(current_2, lup, lup)
-    leg3(current_3, lup, lup)
-    leg4(current_4, lup, lup)
+        leg_4 = ldown
+    leg1(current_1, leg_1, leg_1)
+    leg2(current_2, leg_2, leg_2)
+    leg3(current_3, leg_3, leg_3)
+    leg4(current_4, leg_4, leg_4)
     sleep(1)
 
 
