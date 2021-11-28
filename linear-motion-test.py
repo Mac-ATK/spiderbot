e045a409a51f459513be39b970deb7c469df677f
Mac-ATK <mac.bowley@gmail.com>
2021-11-28 14:10:37 +0000

    adding linear motion

---
+++ linear-motion-test.py
@@ -0,0 +1,11 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+from time import sleep
+
+position_1 = [180, 140, 140]
+
+kit.servo[0].angle = 180 - position_1[0]
+kit.servo[1].angle = 180 - position_1[1]
+kit.servo[2].angle = 180 - position_1[2]
+
+
